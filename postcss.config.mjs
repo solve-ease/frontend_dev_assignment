814b760d49d63fe23980c7d2c59f8416a2127760
Rinki Pargai <rinkisinghpargai@gmail.com>
2025-09-25 12:49:00 +0530

    new message

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,10 @@
 // Next.js expects PostCSS config to be either an array of plugin functions
 // or an object mapping plugin names to options. For Tailwind v4, use the
 // official '@tailwindcss/postcss' plugin via name mapping.
-export default {
+const config = {
   plugins: {
     '@tailwindcss/postcss': {},
   },
 }
+
+export default config
